577f14752459b3f92291f5877c1e4d8874383d05
Kevin Don <kevintang002@gmail.com>
2018-10-11 17:15:46 +0800

    提交Vue代码

---
+++ web/js/custom.js
@@ -0,0 +1,36 @@
+$(function () {
+    /**
+     *  实例化VueJS
+     * @type {Vue}
+     */
+    var app = new Vue({
+        <!-- el指定的ID为css选择器-->
+        el:'#demo',
+        <!-- 变量统统放在data中-->
+        data: {
+            msg: 'Kevin ',
+            Show: true,
+            Hide: false,
+            newitem: {text:''},
+            items : [
+                {text: 'Hello Kevin', },
+            ]
+        },
+        methods: {
+            doThis: function (e){
+                console.log(this);
+                console.log(e.target);
+                console.log('Hello ' + this.msg + '!');
+            },
+            additem: function(newitem){
+                this.items.push(newitem);
+                this.newitem = {text: ''}
+            },
+            deleteItem: function(index, e){
+                this.items.splice(index, 1);
+            }
+        }
+    });
+
+
+})
