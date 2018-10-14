b73a2d1500144c5606e8345f8f1a682c2c0e9cd7
Kevin Don <kevintang002@gmail.com>
2018-10-14 13:32:29 +0800

    提交Commands代码

---
+++ web/js/commands.js
@@ -0,0 +1,19 @@
+$(function () {
+    /**
+     *  实例化VueJS
+     * @type {Vue}
+     */
+    var app = new Vue({
+        el: '#app',
+        data : {
+            message : '这是测试'
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
+
+    })
+});
