85e48b8fa2b79985706252b92d025396c80bcc10
Kevin Don <kevintang002@gmail.com>
2018-10-22 11:23:47 +0800

    提交Project  A代码
    提交Project  A代码

---
+++ web/js/projectA/index.js
@@ -0,0 +1,15 @@
+$(function(){
+    var app = new Vue({
+        el : '#app',
+        data : {
+            message: 'Hello World',
+            show: false
+        },
+        methods: {
+
+        },
+        mounted: function(){
+
+        }
+    });
+});
